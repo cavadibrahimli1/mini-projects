584746e6235ee21d895a77db9128f54229c49c97
Javad Ibrahimli <cenabibrahimov@gmail.com>
2022-02-13 12:29:07 +0400

    Update and rename python_execution_time_calculator to python_execution_time_calculator.py

---
+++ python_execution_time_calculator.py
@@ -0,0 +1,14 @@
+from time import time
+start = time()
+
+#Acronyms maker
+word = "Artificial Intelligence"
+text = word.split()
+a = " "
+for i in text:
+    a = a + str(i[0]).upper()
+print(a)
+
+end = time()
+execution_time = end - start
+print("Execution sime : ", execution_time)
