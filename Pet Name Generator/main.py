6c12d9ef14570617fb9fead31e53775d152a7278
Javad Ibrahimli <cenabibrahimov@gmail.com>
2023-08-18 00:31:39 +0400

    Create main.py

---
+++ Pet Name Generator/main.py
@@ -0,0 +1,5 @@
+print("Welcome to The Band Name Generator!\n")
+print("Let's start!")
+city_name = input("What city did you grow up in?\n")
+pet_name = input("What is the name of your first pet?\n")
+print("Your band name is " + city_name +  " " + pet_name)
